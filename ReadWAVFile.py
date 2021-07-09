307a92c513ecbe00de67a62021adfc1bc44fd83c
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-09 11:23:09 +0100

    Added code for parse RIFF chunk of .wav file

---
+++ ReadWAVFile.py
@@ -0,0 +1,64 @@
+from os import read
+import struct
+import sys
+
+RIFFChunkBytes = []
+fmtSubChunkBytes = []
+dataSubChunkBytes = []
+
+chunkID = ""
+chunkSize = 0
+chunkFormat = ""
+
+subChunk1ID = ""
+subChunk1Size = 0
+audioFormat = ""
+numChannels = 0
+sampleRate = 0
+byteRate = 0
+blockAlign = 0
+bitsPerSample = 0
+
+def ReadFile(fileName) :
+    audioFile = open(fileName, "rb")
+    byte = audioFile.read(1)
+    pos = 0
+    while byte :
+        if pos < 12 :
+            RIFFChunkBytes.append(byte)
+        elif pos < 36 :
+            fmtSubChunkBytes.append(byte)
+        else :
+            dataSubChunkBytes.append(byte)
+        pos = pos + 1
+        byte = audioFile.read(1)
+    __ParseRIFFChunk()
+
+    print("Chunk ID : " + chunkID)
+    print("Chunk Size : " + str(chunkSize))
+    print("Chunk Format : " + chunkFormat)
+
+def __ParseRIFFChunk() :
+    pos = 0
+    chunkSizeBytes = None
+    _chunkID = ""
+    _chunkFormat = ""
+    for byte in RIFFChunkBytes :
+        if pos < 4 :
+            _chunkID = _chunkID + byte.decode("utf-8")
+        elif pos < 8 :
+            if (chunkSizeBytes == None) :
+                chunkSizeBytes = byte
+            else :
+                chunkSizeBytes = chunkSizeBytes + byte
+        else :
+            _chunkFormat = _chunkFormat + byte.decode("utf-8")
+        pos = pos + 1
+
+    global chunkID, chunkSize, chunkFormat
+    chunkID = _chunkID
+    chunkSize = struct.unpack_from("<I", chunkSizeBytes)[0]
+    chunkFormat = _chunkFormat
+
+def __ParseFMTSubChunk() :
+    pass
