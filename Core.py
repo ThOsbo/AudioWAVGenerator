dc74f6175ad48ab34d1decd308dfa21c4d82375f
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-12 12:46:11 +0100

    Added code for testing

--- Core.py
+++ Core.py
@@ -4,3 +4,5 @@ import WriteWAVFile
 if __name__ == "__main__" :
     ReadWAVFile.ReadFile("TestAudio\sound.wav")
     WriteWAVFile.WriteWAVFile()
+    print("\n")
+    ReadWAVFile.ReadFile("TestAudio\TestFile.wav")
