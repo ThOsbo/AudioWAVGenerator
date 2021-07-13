4780bbd03be18631cbc674692b8e59793a957d6d
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-13 12:27:24 +0100

    Updated tests

--- Core.py
+++ Core.py
@@ -1,4 +1,5 @@
 import ByteFiles
+import GenerateWave
 
 if __name__ == "__main__" :
     testFileRead = ByteFiles.wavFile("TestAudio\sound.wav")
@@ -8,4 +9,11 @@ if __name__ == "__main__" :
     testFileWrite = ByteFiles.wavFile("TestAudio\TestFile.wav")
     testFileWrite.WriteFile()
     testFileWrite.PrintFile()
+    print("\n")
+
+    testFileWrite2 = ByteFiles.wavFile("TestAudio\TestFile2.wav")
+    testFileWrite2.ClearAudioData()
+    testFileWrite2.AddToAudioData(GenerateWave.GenerateSquareWave(5, testFileWrite2.sampleRate))
+    testFileWrite2.WriteFile()
+    testFileWrite2.PrintFile()
     print("\n")
