f886730d247acedd0044f63c4206b31742b67675
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-18 14:50:51 +0100

    Updated tests

--- Core.py
+++ Core.py
@@ -1,6 +1,5 @@
 import ByteFiles
 import GenerateWave
-import Filters
 import Instruments
 
 def WriteWaveToFile(wave, sampleRate, fileName) :
@@ -13,46 +12,6 @@ def WriteWaveToFile(wave, sampleRate, fileName) :
     file.close()
 
 if __name__ == "__main__" :
-    # testFileRead = ByteFiles.wavFile("TestAudio\sound.wav")
-    # testFileRead.PrintFile()
-    # print("\n")
-
-    # testFileWrite = ByteFiles.wavFile("TestAudio\TestFile.wav")
-    # testFileWrite.WriteFile()
-    # testFileWrite.PrintFile()
-    # print("\n")
-
-    # testSquareFile = ByteFiles.wavFile("TestAudio\SquareWave.wav")
-    # testSquareFile.ClearAudioData()
-    # testSquareFile.AddToAudioData(GenerateWave.SquareWave(5, testSquareFile.sampleRate, 500))
-    # testSquareFile.WriteFile()
-    # testSquareFile.PrintFile()
-    # print("\n")
-
-    # testSawtoothFile = ByteFiles.wavFile("TestAudio\SawtoothWave.wav")
-    # testSawtoothFile.ClearAudioData()
-    # testSawtoothFile.AddToAudioData(GenerateWave.SawtoothWave(5, testSawtoothFile.sampleRate, 500))
-    # testSawtoothFile.WriteFile()
-    # testSawtoothFile.PrintFile()
-    # print("\n")
-
-    # testTriangleFile = ByteFiles.wavFile("TestAudio\TriangleWave.wav")
-    # testTriangleFile.ClearAudioData()
-    # testTriangleFile.AddToAudioData(GenerateWave.TriangleWave(5, testTriangleFile.sampleRate, 500))
-    # testTriangleFile.WriteFile()
-    # testTriangleFile.PrintFile()
-    # print("\n")
-
-    # testSynthesis = ByteFiles.wavFile("TestAudio\SynthesisSawWave.wav")
-    # filter = Filters.LowPass(500, 0.5)
-    # testSynthesis.ClearAudioData()
-    # sawtoothWave = GenerateWave.SawtoothWave(5, testSynthesis.sampleRate, 250, 5000)
-    # filteredWave = filter.FilterWave(sawtoothWave, testSynthesis.sampleRate)
-    # WriteWaveToFile(sawtoothWave, testSynthesis.sampleRate, "PlotWaves\ToFilterSawWave.txt")
-    # WriteWaveToFile(filteredWave, testSynthesis.sampleRate, "PlotWaves\FilteredSawWave.txt")
-    # testSynthesis.AddToAudioData(filteredWave)
-    # testSynthesis.WriteFile()
-    # testSynthesis.PrintFile()
 
     duration = 2
     frequency = 440
