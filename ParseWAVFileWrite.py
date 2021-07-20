0c14f5f144a5b7569e712fa3a30afc578553f56b
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-20 16:08:33 +0100

    Fixed bug so always converting ints to bytes

--- ParseWAVFileWrite.py
+++ ParseWAVFileWrite.py
@@ -3,30 +3,30 @@ import struct
 def WriteRIFFChunk(chunkID, chunkSize, chunkFormat) :
     bytesToWrite = []
     bytesToWrite.append(chunkID.encode("utf-8"))
-    bytesToWrite.append(struct.pack("<I", chunkSize))
+    bytesToWrite.append(struct.pack("<I", int(chunkSize)))
     bytesToWrite.append(chunkFormat.encode("utf-8"))
     return bytesToWrite
 
 def WriteFmtChunk(subChunk1ID, subChunk1Size, audioFormat, numChannels, sampleRate, byteRate, blockAlign, bitsPerSample) :
     bytesToWrite = []
     bytesToWrite.append(subChunk1ID.encode("utf-8"))
-    bytesToWrite.append(struct.pack("<I", subChunk1Size))
-    bytesToWrite.append(struct.pack("<H", audioFormat))
-    bytesToWrite.append(struct.pack("<H", numChannels))
-    bytesToWrite.append(struct.pack("<I", sampleRate))
-    bytesToWrite.append(struct.pack("<I", byteRate))
-    bytesToWrite.append(struct.pack("<H", blockAlign))
-    bytesToWrite.append(struct.pack("<H", bitsPerSample))
+    bytesToWrite.append(struct.pack("<I", int(subChunk1Size)))
+    bytesToWrite.append(struct.pack("<H", int(audioFormat)))
+    bytesToWrite.append(struct.pack("<H", int(numChannels)))
+    bytesToWrite.append(struct.pack("<I", int(sampleRate)))
+    bytesToWrite.append(struct.pack("<I", int(byteRate)))
+    bytesToWrite.append(struct.pack("<H", int(blockAlign)))
+    bytesToWrite.append(struct.pack("<H", int(bitsPerSample)))
     return bytesToWrite
 
 def WriteDataChunk(subChunk2ID, subChunk2Size, data, bitsPerSample) :
     bytesToWrite = []
     bytesToWrite.append(subChunk2ID.encode("utf-8"))
-    bytesToWrite.append(struct.pack("<I", subChunk2Size))
+    bytesToWrite.append(struct.pack("<I", int(subChunk2Size)))
     for val in data :
         if bitsPerSample == 8 :
-            byte = bytes([val])
+            byte = bytes([int(val)])
         elif bitsPerSample == 16 :
-            byte = struct.pack("<h", val)
+            byte = struct.pack("<h", int(val))
         bytesToWrite.append(byte)
     return bytesToWrite
