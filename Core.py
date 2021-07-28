2807799752bde45182b1f160aad10522ca675a97
Spirit1230 <tom.osborn1@btinternet.com>
2021-07-28 15:47:09 +0100

    Updated tests

--- Core.py
+++ Core.py
@@ -2,6 +2,7 @@ import ByteFiles
 import GenerateWave
 import Instruments
 import Music
+import struct
 
 def WriteWaveToFile(wave, sampleRate, fileName) :
     file = open(fileName, "w")
@@ -14,73 +15,69 @@ def WriteWaveToFile(wave, sampleRate, fileName) :
 
 if __name__ == "__main__" :
 
-    # duration = 2
-    # frequency = 440
-    # testViolin = ByteFiles.wavFile("TestAudio\TestViolinWave.wav")
-    # baseWave = GenerateWave.SawtoothWave(duration, testViolin.sampleRate, frequency)
-    # WriteWaveToFile(baseWave, testViolin.sampleRate, "PlotWaves\BaseSawtoothWave.txt")
-    # tempViolin = Instruments.Violin(testViolin.sampleRate)
-    # notes = ["A0", "E1", "A1", "E2", "A2", "E3", "A440", "E4", "A4", "E5", "A5", "E6", "A6"]
-    # testViolin.ClearAudioData()
-    # for note in notes :
-    #     tempViolinWave = tempViolin.GetWave(note, duration)
-    #     WriteWaveToFile(tempViolinWave, testViolin.sampleRate, "PlotWaves\ViolinTest" + note + "Wave.txt")
-    #     testViolin.AddToAudioData(tempViolinWave)
-    # testViolin.WriteFile()
-    # testViolin.PrintFile()
+    # drumViolinFile = ByteFiles.wavFile("TestAudio\\TestDrumViolinDuet.wav")
 
-    # beatsPerMinute = 100
-    # testMusicFile = ByteFiles.wavFile("TestAudio\TestMusicScore.wav")
-    # MusicSheet = Music.Sheet(beatsPerMinute, testMusicFile.sampleRate)
-    # MusicSheet.AddStave("Violin")
-    
-    # violinStave = MusicSheet.GetStave("Violin")
-    # violinStave.AddNote("A440", 4)
-    # violinStave.AddNote("E3", 1)
-    # violinStave.AddNote("A2", 1)
-    # violinStave.AddNote("E3", 0.5)
-    # violinStave.AddNote("A440", 0.5)
-    # violinStave.AddNote("E4", 1)
-    # violinStave.AddNote("A440", 4)
+    # testViolin = Instruments.Violin(drumViolinFile.sampleRate)
+    # testDrum = Instruments.Drum(drumViolinFile.sampleRate)
 
-    # musicSheetWave = MusicSheet.GetWave()
-    # WriteWaveToFile(musicSheetWave, testMusicFile.sampleRate, "PlotWaves\TestMusicWave.txt")
+    # WriteWaveToFile(testViolin.GetWave("A0", 1), drumViolinFile.sampleRate, "PlotWaves\\TestViolinLowNote.txt")
+    # WriteWaveToFile(testDrum.GetWave("A1", 1), drumViolinFile.sampleRate, "PlotWaves\\TestDrumLowNote.txt")
 
-    # testMusicFile.ClearAudioData()
-    # testMusicFile.AddToAudioData(musicSheetWave)
-    # testMusicFile.WriteFile()
-    # testMusicFile.PrintFile()
+    # musicSheet = Music.Sheet(100, drumViolinFile.sampleRate)
+    # musicSheet.AddStave("Violin", "Violin")
+    # musicSheet.AddStave("Drum", "Drum", 10)
 
-    beatsPerMinute = 100
-    testMusicFile = ByteFiles.wavFile("TestAudio\TestMusicDuetScore.wav")
-    MusicSheet = Music.Sheet(beatsPerMinute, testMusicFile.sampleRate)
-    MusicSheet.AddStave("Violin", "Violin1")
-    MusicSheet.AddStave("Violin", "Violin2")
-    
-    violinStave = MusicSheet.GetStave("Violin1")
-    violinStave.AddNote("A440", 4)
-    violinStave.AddNote("E3", 1)
-    violinStave.AddNote("A2", 1)
-    violinStave.AddNote("E3", 0.5)
-    violinStave.AddNote("A440", 0.5)
-    violinStave.AddNote("E4", 1)
-    violinStave.AddNote("A440", 4)
+    # staveViolin = musicSheet.GetStave("Violin")
+    # staveViolin.AddNote("E3", 1)
+    # staveViolin.AddNote("A440", 1)
+    # staveViolin.AddNote("E3", 1)
+    # staveViolin.AddNote("A2", 1)
 
-    violinStave2 = MusicSheet.GetStave("Violin2")
-    violinStave2.AddBreak(4)
-    violinStave2.AddNote("A440", 4)
-    violinStave2.AddNote("E3", 1)
-    violinStave2.AddNote("A2", 1)
-    violinStave2.AddNote("E3", 0.5)
-    violinStave2.AddNote("A440", 0.5)
-    violinStave2.AddNote("E4", 1)
-    violinStave2.AddNote("A440", 4)
+    # staveDrum = musicSheet.GetStave("Drum")
+    # staveDrum.AddNote("A1", 1)
+    # staveDrum.AddNote("A1", 1)
+    # staveDrum.AddNote("A1", 1)
+    # staveDrum.AddNote("A1", 1)
 
-    musicSheetWave = MusicSheet.GetWave()
-    WriteWaveToFile(musicSheetWave, testMusicFile.sampleRate, "PlotWaves\TestMusicDuetWave.txt")
+    # musicSheetWave = musicSheet.GetWave()
 
-    testMusicFile.ClearAudioData()
-    testMusicFile.AddToAudioData(musicSheetWave)
-    testMusicFile.WriteFile()
-    testMusicFile.PrintFile()
-    
+    # drumViolinFile.ClearAudioData()
+    # drumViolinFile.AddToAudioData(musicSheetWave)
+    # drumViolinFile.WriteFile()
+    # drumViolinFile.PrintFile()
+
+    # drumFile = ByteFiles.wavFile("TestAudio\\TestDrum.wav")
+    # drumSheet = Music.Sheet(100, drumFile.sampleRate)
+    # drumSheet.AddStave("Drum")
+
+    # testDrumStave = drumSheet.GetStave("Drum")
+    # testDrumStave.AddNote("A1", 1)
+    # testDrumStave.AddNote("C2", 1)
+    # testDrumStave.AddNote("D2", 1)
+    # testDrumStave.AddNote("F2", 1)
+    # testDrumStave.AddNote("G2", 1)
+    # testDrumStave.AddNote("B3", 1)
+    # testDrumStave.AddNote("E3", 1)
+
+    # testDrumWave = drumSheet.GetWave()
+    # WriteWaveToFile(testDrumWave, drumFile.sampleRate, "PlotWaves\\TestDrum.txt")
+
+    # drumFile.ClearAudioData()
+    # drumFile.AddToAudioData(testDrumWave)
+    # drumFile.WriteFile()
+    # drumFile.PrintFile()
+
+    const_h_max = 32767
+    const_f_max = struct.unpack_from("<f", b'\xff\xff\x7f\x7f')[0]
+
+    testFile = ByteFiles.wavFile("TestAudio\\TestByteSize.wav")
+    testFile.SetBitsPerSample(32)
+
+    testWave = GenerateWave.SquareWave(5, testFile.sampleRate, 100, const_f_max)
+    WriteWaveToFile(testWave, testFile.sampleRate, "PlotWaves\\TestByteSize.txt")
+    WriteWaveToFile(testFile.data, testFile.sampleRate, "PlotWaves\\TestByteSizeRead.txt")
+
+    testFile.ClearAudioData()
+    testFile.AddToAudioData(testWave)
+    testFile.WriteFile()
+    testFile.PrintFile()
